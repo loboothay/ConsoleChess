62929f7c36b2c2f54b72e33bd7cf07a8e15c9a0d
Thaynara Lobo <loboo.thaynara@gmail.com>
2023-01-02 15:18:06 -0300

    Creating the class ChessPosition

---
+++ ConsoleChess/Chess/ChessPosition.cs
@@ -0,0 +1,31 @@
+﻿using DirBoard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class ChessPosition
+    {
+        public ChessPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public Position toPosition()
+        {
+            return new Position(8-Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
